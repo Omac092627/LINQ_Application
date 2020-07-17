9d293a3cef86cdfa9980fb84324b070e1aadb148
Olamide Ogunde <oogunde0413@gmail.com>
2020-07-16 19:13:22 -0700

    get elements with names

--- LINQ_IN_MANHATTAN/Program.cs
+++ LINQ_IN_MANHATTAN/Program.cs
@@ -10,34 +10,47 @@ namespace LINQ_IN_MANHATTAN
 {
     class Program
     {
-        public static Root JsonData {get; set;}
+        public static Root JsonData { get; set; }
 
-    static void Main(string[] args)
-    {
+        static void Main(string[] args)
+        {
             GetJson();
             var GetNeighborhoods = from place in JsonData.features
-            select place.properties.neighborhood;
-    }
+                                   select place.properties.neighborhood;
+
+            for(int i = 0; i < GetNeighborhoods.Count(); i++)
+            {
+                Console.WriteLine($"{i + 1}.{GetNeighborhoods.ElementAt(i)}");
+            }
+
+
+            var GetNeighborhoodsWithName = from place in JsonData.features
+                                              where place.properties.neighborhood != ""
+                                              select place.properties.neighborhood;
+
+            for(int i = 0; i < GetNeighborhoodsWithName.Count(); i++)
+            {
+                Console.WriteLine($"{i + 1}. {GetNeighborhoodsWithName.ElementAt(i)}");
+            }
+        }
 
         public static void GetJson()
         {
             string rawData = File.ReadAllText("../../../Assets/data.json");
             JsonData = JsonConvert.DeserializeObject<Root>(rawData);
-
+            int count = 0;
             foreach (var item in JsonData.features)
             {
-                Console.WriteLine(item.properties.neighborhood);
+                count++;
+                Console.WriteLine(item.properties.neighborhood + count);
             }
         }
 
-}
-
-
-
- 
-
+        }
 
     }
+
+
 
 
 
